a0216cd00fbc29b515e36db00443aed3d687459b
rubyonrials <matthew.rials+github2@gmail.com>
2023-08-07 13:55:25 -0700

    working server-side synchronization!!

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 6900;
 
@@ -10,7 +11,38 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static('source-hls'));
+let startTime;
+getStartOffset = () => {
+  if (!startTime) startTime = new Date();
+  let offset = (new Date() - startTime) / 1000;
+  return offset;
+};
+
+app.get('/stream', (req, res) => {
+  fs.readFile(`${__dirname}/hls-manifest/blue.m3u8`, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error reading playlist file");
+      return;
+    }
+
+    let lines = data.split('\n');
+    let playlist = '';
+    for (let i = 0; i < lines.length; i++) {
+      if (lines[i] === '#EXT-X-VERSION:3') {
+        playlist += lines[i] + '\n';
+        playlist += `#EXT-X-START:TIME-OFFSET=${getStartOffset()},PRECISE=YES\n`;
+      } else {
+        playlist += lines[i] + '\n';
+      }
+    }
+
+    res.set('Content-Type', 'application/vnd.apple.mpegurl');
+    res.send(playlist);
+  });
+});
+
+app.use(express.static('hls-data'));
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World!');
