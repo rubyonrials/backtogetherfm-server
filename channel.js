6a25364d08814fbfda5b9431c6a7a6c5a847715a
rubyonrials <matthew.rials+github2@gmail.com>
2024-03-24 23:48:11 -0400

    thing

---
+++ channel.js
@@ -0,0 +1,124 @@
+const fs = require('fs').promises;
+const { v4: uuidv4 } = require('uuid');
+
+const RED = 'red';
+const GREEN = 'green';
+const BLUE = 'blue';
+const YELLOW = 'yellow';
+
+// TODO: Eventually we will send color information to the client, when there are multiple channels available at once again
+// TODO: Why would server be responsible for setting the actual hex color values? So that client(s) does not have to be updated when a party wants to add a new/different color. Server is the easiest to update and control, esp in SDB land. Dumb client, even though it's strange setting hex values here
+const COLOR_MAP = {
+	[ YELLOW ]: {
+		colorOpaque:'#ffb100ba',
+		colorTransparent:'#ffb10042'
+	},
+	[ RED ]: {
+		colorOpaque:'#dc322fba',
+		colorTransparent:'#dc322f42'
+	},
+	[ GREEN ]: {
+		colorOpaque:'#429900ba',
+		colorTransparent:'#45ff0042'
+	},
+	[ BLUE ]: {
+		colorOpaque:'#268bd2ba',
+		colorTransparent:'#268bd242'
+	}
+};
+
+// NOTE: ID is how channels are distinguished from one another. A channel can continue with multiple source manifests and still be the same channel to a user ("green channel" that plays multiple groovy sets)
+// source: manifest filename in hls-data/
+// livestreaming: Boolean
+// ibeaconMinor: the minor value expected on an ibeacon for MULTICHANNEL-SPATIAL client mode
+// color: COLOR_MAP keys (YELLOW | RED | GREEN | BLUE)
+const createChannel = async ({
+	source,
+	livestreaming,
+	ibeaconMinor = null,
+	color
+}) => {
+	let playbackTimer = null;
+
+	const getManifestLines = async () => {
+		const manifestPath = `hls-data/${source}`;
+		const manifestData = await fs.readFile(`${__dirname}/${manifestPath}`, 'utf8');
+		const manifestLines = manifestData.split('\n');
+		return manifestLines;
+	};
+
+	// TODO: Could cache this for non-livestreaming files? So that when isExpired gets called (for every client calling /getStreamableChannels, and every time ensureBroadcasting starts a stream) we don't have to do a file read
+	const getDuration = async () => {
+		const duration = await getManifestLines()
+			.filter(line => line.startsWith('#EXTINF:'))
+			.map(line => parseFloat(line.split(':')[1]))
+			.reduce((a, b) => a + b, 0);
+		return duration;
+	};
+
+	const getPlaybackOffset = () => {
+		let playbackOffset = (new Date() - playbackTimer) / 1000;
+		return playbackOffset;
+	};
+
+	const isExpired = async () => {
+		if(!playbackTimer) return false;
+		if(livestreaming) return false; //TODO we need to eventually set this to false for livestreams
+
+		const duration = await getDuration();
+		const isExpired = getPlaybackOffset() >= duration;
+		return isExpired;
+	}
+
+	const ensureBroadcasting = async () => {
+		if ( playbackTimer !== null ) return;
+
+		playbackTimer = new Date();
+
+		setTimeout(() => {
+			if(!(await isExpired())) {
+				console.error("ensureBroadcasting cleanup expected expired broadcast."); // TODO why do we need this? is there a better way?
+				return;
+			}
+
+			reset();
+		}, (await getDuration()) * 1000);
+	};
+
+	const reset = (newSource = null) => {
+	  if (newSource) {
+		  if (newSource === source) {
+			  throw new Error("Cannot reset; 'newSource' parameter is the same as the existing 'source'.");
+		  }
+
+		  source = newSource;
+	  }
+
+	  playbackTimer = null;
+	};
+
+	const initialize = async () => {
+		if (!source) throw new Error("Cannot createChannel without 'source' parameter.");
+		if (livestreaming === undefined) throw new Error("Cannot createChannel without 'livestreaming' parameter.");
+		if (!color) throw new Error("Cannot createChannel without 'color' parameter.");
+		if (!Object.keys(COLOR_MAP).includes(color)) {
+			throw new Error(`Cannot createChannel; 'color' parameter must be one of ${Object.keys(COLOR_MAP).join(', ')}`);
+		}
+
+		return {
+			id: uuidv4(),
+			source,
+			livestreaming,
+			ibeaconMinor,
+			color,
+			colorOpaque: COLOR_MAP[color].colorOpaque,
+			colorTransparent: COLOR_MAP[color].colorTransparent,
+			getManifestLines,
+			getPlaybackOffset,
+			isExpired,
+			reset
+		};
+	};
+
+	return await initialize();
+};
--- index.js
+++ index.js
@@ -4,13 +4,8 @@ const app = express();
 const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 9876;
-const RED = 'red';
-const GREEN = 'green';
-const BLUE = 'blue';
-const YELLOW = 'yellow';
-const STATIC = 'STATIC';
-const LIVESTREAM = 'LIVESTREAM';
-const ERROR_DELIMITER = ':::';
+
+let channels = [];
 
 // Middleware to set CORS headers
 app.use((req, res, next) => {
@@ -26,221 +21,39 @@ app.use((req, res, next) => {
   next();
 });
 
-// let broadcasts = {
-//   [ RED ]: {
-//     source: null,
-//     playbackTimer: null,
-//     duration: null,
-//     // TODO livestream: false
-//   },
-//   [ GREEN ]: {
-//     source: null,
-//     playbackTimer: null,
-//     duration: null,
-//     // TODO livestream: false
-//   },
-//   [ BLUE ]: {
-//     source: null,
-//     playbackTimer: null,
-//     duration: null,
-//     // TODO livestream: false
-//   }
-// }; // TODO this should probably be a class, with instantiated broadcasts, with types that can be checked. refreshCHannel is really initialize, and initializes the instance variables
-
-// TODO: Eventually we will send color information to the client, when there are multiple channels available at once again
-const COLOR_MAP = {
-	[ YELLOW ]: {
-		colorOpaque:'#ffb100ba',
-		colorTransparent:'#ffb10042'
-	},
-	[ RED ]: {
-		colorOpaque:'#dc322fba',
-		colorTransparent:'#dc322f42'
-	},
-	[ GREEN ]: {
-		colorOpaque:'#429900ba',
-		colorTransparent:'#45ff0042'
-	},
-	[ BLUE ]: {
-		colorOpaque:'#268bd2ba',
-		colorTransparent:'#268bd242'
-	}
-};
-
-// id: UUID. Represents what the current channel is to the client, and whether it has changed
-// type: STATIC | LIVESTREAM
-// source: relative path to hls-source/
-// ibeaconMinor: the minor value expected on an ibeacon for MULTICHANNEL-SPATIAL client mode
-// color: COLOR_MAP keys (YELLOW | RED | GREEN | BLUE)
-const createBroadcast = async ({
-	type,
-	source,
-	ibeaconMinor = null,
-	color
-}) => {
-	let duration = null;
-	let playbackTimer = null;
-
-	const setBroadcastDuration = async () => {
-		const sourceManifestPath = `hls-manifest/source/${source}`;
-		const sourceManifestData = await fs.readFile(`${__dirname}/${sourceManifestPath}`, 'utf8');
-		const sourceManifestLines = sourceManifestData.split('\n');
-		const broadcastDuration = sourceManifestLines
-			.filter(line => line.startsWith('#EXTINF:'))
-			.map(line => parseFloat(line.split(':')[1]))
-			.reduce((a, b) => a + b, 0);
-		duration = broadcastDuration;
-	};
-
-	const refresh = async (newSource = null) => {
-	  if (newSource) {
-		  if (newSource === source) {
-			  throw new Error("Cannot refresh; 'newSource' parameter is the same as the existing 'source'.");
-		  }
-
-		  source = newSource;
-	  }
-
-	  playbackTimer = null;
-	  await setBroadcastDuration();
-	};
-
-	const initialize = async () => {
-		if (!type) throw new Error("Cannot createBroadcast without 'type' parameter.");
-		if (![STATIC, LIVESTREAM].includes(type)) throw new Error(`Cannot createBroadcast; 'type' must be one of [${STATIC}, ${LIVESTREAM}]`);
-		if (!source) throw new Error("Cannot createBroadcast without 'source' parameter.");
-		if (!color) throw new Error("Cannot createBroadcast without 'color' parameter.");
-		if (!Object.keys(COLOR_MAP).includes(color)) {
-			throw new Error(`Cannot createBroadcast; 'color' parameter must be one of ${Object.keys(COLOR_MAP).join(', ')}`);
-		}
-
-		// Set broadcast duration if type STATIC
-		if (type === STATIC) {
-			await setBroadcastDuration();
-		}
-
-		return {
-			id: uuidv4(),
-			type,
-			source,
-			ibeaconMinor,
-			color,
-			duration,
-			playbackTimer,
-			refresh
-		};
-	};
-
-	return await initialize();
-};
-
-const getSourceManifestLines = async (channel) => {
-  const sourceManifestPath = `hls-manifest/source/${channel}/${broadcasts[channel].source}`;
-  const sourceManifestData = await fs.readFile(`${__dirname}/${sourceManifestPath}`, 'utf8');
-  const sourceManifestLines = sourceManifestData.split('\n');
-  return sourceManifestLines;
-};
-
-const getPlaybackOffset = (channel) => {
-  const playbackTimer = broadcasts[channel].playbackTimer;
-  let playbackOffset = (new Date() - playbackTimer) / 1000;
-  return playbackOffset;
-};
-
-const isBroadcastExpired = (channel) => {
-  const { playbackTimer, duration } = broadcasts[channel];
-  if(!playbackTimer) return false;
-  if(!duration) {
-    console.error("isBroadcastExpired expected duration to be set."); // TODO
-    return false;
-  }
-  const broadcastExpired = getPlaybackOffset(channel) >= duration;
-  return broadcastExpired;
-};
-
-// channel = RED | GREEN | BLUE
-// newSource = string | null
-const refreshChannel = async (channel, newSource = null) => {
-  // Cache the duration when broadcast is refreshed / loaded so we don't have to
-  // calculate it every time a new client checks for broadcastable channels
-  // returns: nothing
-  const setBroadcastDuration = async (channel) => {
-    const sourceManifestLines = await getSourceManifestLines(channel);
-    const broadcastDuration = sourceManifestLines
-      .filter(line => line.startsWith('#EXTINF:'))
-      .map(line => parseFloat(line.split(':')[1]))
-      .reduce((a, b) => a + b, 0);
-    broadcasts[channel].duration = broadcastDuration;
-  };
-
-  // returns: nothing
-  const clearOldClientManifests = async (channel) => {
-    try {
-      const clientManifestPath = `${__dirname}/hls-manifest/client`;
-      const clientManifestFiles = await fs.readdir(clientManifestPath);
-      for (const filename of clientManifestFiles) {
-        if (!filename.startsWith(channel)) continue;
-        fs.unlink(`${clientManifestPath}/${filename}`); // async
-      }
-    } catch (err) {
-      console.error(`Error removing client manifests for channel ${channel}`);
-    }
-  };
-
-  // Make sure source is changed before clearing playbackTimer, otherwise a new client could begin a stream
-  // of the old source with a newly-cleared playbackTimer.
-  // This will NOT stop streaming for old clients, who are loading chunks directly from hls-data now. That is handled by getStreamableChannels
-  if (newSource) broadcasts[channel].source = newSource;
-  broadcasts[channel].playbackTimer = null;
-  setBroadcastDuration(channel); // async
-  clearOldClientManifests(channel); // async
-};
-
 // NEW METHOD: every 10 seconds, check current streams. if any are expired, refresh them.
 
-// Return the channel names that are streamable as an array.
-// A channel is streamable if it's playbackTimer is not null (not yet started broadcast) or expired
-app.get('/getStreamableChannels', (req, res) => {
-  // could check if broadcast is expired; if so, refresh it. let's let the timeout handle this for now
-  // plus a regular check for livestreams, or odd jobs
-  // for example, if the server shuts down when it's supposed to refresh a stream, it will go zombie mode
-  const allChannels = [RED, GREEN, BLUE];
-  const streamableChannels = allChannels.filter(channel => {
-    if(!broadcasts[channel].source) return false;
-    return !isBroadcastExpired(channel);
-    // TODO could also show whether the broadcast will be live here!
-    // TODO probably need to show source. if source is different we need to redo things client-side.... emit event when channel source changes
-  });
-
-  // TODO filter out playbackTimer; figure out a way to do this with a deep clone rather than rewriting it myself
-  let obj = {};
-  streamableChannels.map(channel => {
-    obj[channel] = broadcasts[channel];
-  });
-  res.send(obj);
+// Return the channel names that are streamable as an array of channel objects.
+app.get('/getStreamableChannels', async (req, res) => {
+	try {
+		const streamableChannels = channels.filter(channel => {
+			return !(await channel.isExpired());
+		});
+		res.send(streamableChannels);
+	} catch (err) {
+		// TODO DRY up 500 handling for api endpoints
+		console.error('Internal server error: ', err);
+		res.status(500).send('Internal Server Error');
+	}
 });
 
-// req.params.channel = RED | GREEN | BLUE
-// Create a temporary, unique manifest for the client with a unique playback offset. Return the URL.
-// Note: we don't just serve the manifest directly, because the client's HLS player needs to continually poll for updates to live streams.
+app.get('/:channelSourceName.m3u8', async (req, res) => {
+	try {
+		const channel = channels.filter(channel => {
+			channel.source === `${req.params.channelSourceName}.m3u8`;
+		});
+
+		const manifestLines = await channel.getManifestLines();
+		// TODO TODO TODO
+		// This is where I am. I need to dynamically serve the manifest file, with a new EXT-X-START time as below. If it doesn't exist, add it. If it exists, overwrite it.
+	} catch (err) {
+		console.error('Internal server error: ', err);
+		res.status(500).send('Internal Server Error');
+	}
+});
+
+// TODO delete this
 app.post('/stream/:channel', async (req, res) => {
-  const ensureBroadcasting = (channel) => {
-    if (broadcasts[channel].playbackTimer) return;
-
-    // Beware, if you try to use `const timer = broadcasts[channel].playbackTimer` you won't actually update the timer. Mutable objects!
-    broadcasts[channel].playbackTimer = new Date();
-    broadcastDuration = broadcasts[channel].duration;
-    if (!broadcastDuration) console.error("ensureBroadcasting expected duration to be set."); // TODO
-    setTimeout(() => {
-      if(!isBroadcastExpired(channel)) {
-        console.error("ensureBroadcasting cleanup expected expired broadcast."); // TODO
-        return;
-      }
-      refreshChannel(channel); // async
-    }, broadcastDuration * 1000);
-  };
-
-  const errorPrefix = `POST /stream/:channel ${ERROR_DELIMITER}`;
   const channel = req.params.channel;
   if (!channel || (channel !== RED && channel !== GREEN && channel !== BLUE)) {
     res.status(400).send(`${errorPrefix} Invalid channel parameter (must be one of 'red', 'green', 'blue').`);
@@ -268,11 +81,9 @@ app.post('/stream/:channel', async (req, res) => {
 });
 
 app.use(express.static('hls-data'));
-app.use(express.static('hls-manifest/client'));
 
 app.listen(PORT, () => {
-  refreshChannel(RED, 'bassinfusion.m3u8'); // can move source-chooser to method
-  // refreshChannel(GREEN, 'set1.m3u8'); // can move source-chooser to method
-  // refreshChannel(BLUE, 'set1.m3u8'); // can move source-chooser to method
+  // refreshChannel(RED, 'bassinfusion.m3u8');
+	// TODO redo this
   console.log(`Server is listening on port ${PORT}.`);
 });
